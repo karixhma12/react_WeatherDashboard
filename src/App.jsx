28f016a369a87225d07bfec1209598fea8024a6c
Karishma <karishma.7022@gmail.com>
2026-05-21 10:19:39 +0530

    wire everything together in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
-
+import {useState} from "react";
+import { useSetRecoilState } from "recoil";
+import { favouriteCitiesAtom } from "./store/atoms";
+import SearchBar from "./components/SearchBar";
+import StatusBar from "./components/StatusBar";
+import WeatherCard from "./components/WeatherCard";
+import FavouriteCities from "./components/FavouriteCities";
 
 function App(){
+  const [city,setCity] = useState('London');
+  const setFavourites = useSetRecoilState(favouriteCitiesAtom);
+
+  const addToFavourities = ()=>{
+    setFavourites((prev)=>{
+      if(prev.includes(city)){
+        return prev;
+      }
+      else{
+        return [...prev,city];
+      }
+    })
+  }
+
   return(
-    <h1> Weather Dashboard </h1>
+    <div>
+      <SearchBar onSearch={setCity}/>
+      <StatusBar/>
+      {city && <WeatherCard city={city}/>}
+      <button onClick={addToFavourities}> Add To Favourites </button>
+      <FavouriteCities onCityClick={setCity}/>
+    </div>
   )
 }
 
